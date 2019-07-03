c724e5273524f8d43dae8857dd940737bf4e8f62
vivek joshi <reachvivekjoshi@gmail.com>
2019-07-03 16:09:38 +0530

    added flags to store if the player goes bust or has a blackjack

--- PlayerClass.py
+++ PlayerClass.py
@@ -2,12 +2,13 @@ from PythonTrack import DeckClass
 
 
 class Player():
-    def __init__(self):
+    def __init__(self,  name):
         self.name = name
         self.player_hand = []
         self.total = 0
         self.deal()
         self.bust = False
+        self.black_jack = False
 
     def deal(self):
         self.player_hand.append(DeckClass.Deck.deck.pop(-1))
@@ -23,6 +24,7 @@ class Player():
         print("and points are:" + str(self.total))
 
     def add(self):
+        self.total = 0
         for i in range(len(self.player_hand)):
             value = self.player_hand[i][0]
             if value > 10:
@@ -30,11 +32,12 @@ class Player():
             if value == 1 and self.total <= 10:
                 value = 11
             self.total += value
+        if self.total > 21:
+            self.bust = True
+        if self.total == 21 and len(self.player_hand) == 2:
+            self.black_jack = True
 
     def hit(self):
         self.player_hand.append(DeckClass.Deck.deck.pop(-1))
         self.player_hand.sort(reverse=True)
         self.add()
-        self.show()
-
-
