4be471c09a7fe0a61dbc18b5dde3f76258b0ef1e
vivek joshi <reachvivekjoshi@gmail.com>
2019-07-03 18:26:40 +0530

    bug which messed with the exit condition fixed

--- Black_jack.py
+++ Black_jack.py
@@ -9,20 +9,20 @@ def main():
         for i in range(nop):
             player_name = input("enter name for player" + ' ' + str(i+1) + ':')
             player_objects.append(PlayerClass.Player(player_name))
-        eleminated_players = 0
-        while eleminated_players < len(player_objects):
+        eliminated_players = 0
+        while eliminated_players < len(player_objects):
             for active_player in player_objects:
                 if active_player.bust is False and active_player.final is False:
                     print(active_player.name + '\'s  turn')
                     active_player.show()
-                    choice = input("enter 1 to hit or 2 to stay")
+                    choice = int(input("enter 1 to hit or 2 to stay"))
                     if choice == 1:
                         active_player.hit()
                         if active_player.bust is True:
-                            eleminated_players += 1
+                            eliminated_players = eliminated_players+1
                     if choice == 2:
                         active_player.stay()
-                        eleminated_players += 1
+                        eliminated_players = eliminated_players+1
 
     else:
         print("invalid input")
