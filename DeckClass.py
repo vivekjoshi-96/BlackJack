c2e78a8a854a0c14f21ea41271024ef7b55e22d5
vivek joshi <reachvivekjoshi@gmail.com>
2019-08-07 15:37:32 +0530

    updated to create a shoe contining n decks specified by user

--- DeckClass.py
+++ DeckClass.py
@@ -3,5 +3,16 @@ import random
 
 
 class Deck:
-    deck = list(itertools.product(range(1, 14), ['Spade', 'Heart', 'Diamond', 'Club']))
-    random.shuffle(deck)
+    def __init__(self, n):
+        self.shoe = self.build_decks(n)
+
+    @staticmethod
+    def build_decks(n):
+        decks = []
+        for i in range(n):
+            deck = list(itertools.product(range(1, 14), ['Spade', 'Heart', 'Diamond', 'Club']))
+            decks = decks + deck
+        random.shuffle(decks)
+        return decks
+
+
